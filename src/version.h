d81ef96fda8237a1deae9d93d9e58b2711fb6961
Star Rauchenberger <fefferburbia@gmail.com>
2023-08-25 22:16:14 -0400

    Bump version

--- src/version.h
+++ src/version.h
@@ -35,6 +35,6 @@ std::ostream& operator<<(std::ostream& out, const Version& ver) {
   return out << "v" << ver.major << "." << ver.minor << "." << ver.revision;
 }
 
-constexpr const Version kTrackerVersion = Version(0, 4, 5);
+constexpr const Version kTrackerVersion = Version(0, 5, 0);
 
 #endif /* end of include guard: VERSION_H_C757E53C */
